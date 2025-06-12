dbf022ef4af44dc16a414c4298431f2877085270
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-06-12 23:38:57 +0530

    configured authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,48 +1,17 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const authController = require("../controllers/authController.js");
+import {signUp, signIn} from '../controllers/authController.js';
+import {Router} from "express";
 
-//Middlewares
-const authMiddleware = require("../middlewares/authMiddleware.js");
-const isAdmin = require('../middlewares/isAdmin.js');
+const router = Router();
 
-//admin
-router.post('/admin/signup', authController.adminSignup);
-router.post('/admin/login',authController.adminLogin);
+// User sign-up
+router.post('/sign-up', (req ,res) => {
+    signUp(req, res);
+});
 
-//users
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.post('/logout', authMiddleware, authController.logout);
+router.post('/sign-in', (req, res) => {
+    signIn(req, res);
+});
 
 
-router.get('/me', authMiddleware, authController.getCurrentUser);
+export default router;
 
-// Google OAuth routes
-router.get('/google',
-  passport.authenticate('google', { 
-    scope: ['profile', 'email'],
-    prompt: 'select_account'
-  })
-);
-
-router.get('/google/callback',
-  passport.authenticate('google', { 
-    session: false,
-    failureRedirect: 'http://localhost:5173/login'
-  }),
-  (req, res) => {
-    const token = jwt.sign(
-      { id: req.user.id, email: req.user.email, isAdmin: req.user.isAdmin },
-      process.env.JWT_SECRET,
-      { expiresIn: '1d' }
-    );
-    
-    // Redirect to frontend with token
-    res.redirect(`http://localhost:5173/auth/google/callback?token=${token}`);
-  }
-);
-
-module.exports = router;
