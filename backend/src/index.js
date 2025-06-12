11106014d586091547111be0bd237ca55e04b7a9
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-06-12 23:36:40 +0530

    configured index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,19 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const prisma = require('./utils/prismaImport');
-const errorHandler = require('./middlewares/errorHandler');
-
-// Import routes
-const authRoutes = require('./routes/authRoutes');
-const blogRoutes = require('./routes/blogRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
-//const paymentRoutes = require('./routes/paymentRoutes');
-const plantRoutes = require('./routes/PlantRoutes');
-// const reviewRoutes = require('./routes/reviewRoutes');
-const userRoutes = require('./routes/UserRoutes');
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from "./config/db.js";
 
 // Load environment variables
 dotenv.config();
@@ -21,6 +9,8 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 
+// Database Connection
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -28,21 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/blog', blogRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/categories', categoryRoutes);
-// app.use('/api/orders', orderRoutes);
-// //app.use('/api/payments', paymentRoutes);
-app.use('/api/plants', plantRoutes);
-// app.use('/api/reviews', reviewRoutes);
-app.use('/api/users', userRoutes);
-
-// Error handling middleware
-app.use(errorHandler);
+app.use("/auth", authRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
