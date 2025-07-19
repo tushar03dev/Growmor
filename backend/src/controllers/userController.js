5228fcb21c3b00eb2f0aee7ac96c37c82183df9d
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-19 16:26:35 +0530

    created userController.js

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,28 +1,45 @@
-const prisma = require('../utils/prismaImport.js');
-const asyncHandler = require('../utils/asyncHandler');
+import { User } from '../models/model.js';
+import bcrypt from 'bcryptjs';
 
-exports.getUserProfile = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-    select: { id: true, email: true, name: true }
-  });
-  res.json(user);
-});
+// Get user profile
+export const getUserProfile = async (req, res) => {
+  try {
+    const userId = req.user.id
+    const user = await User.findOne({ _id: userId }).select('_id email name');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
-exports.updateUserProfile = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
-  const data = req.body;
-
-  if (data.password) {
-    const bcrypt = require('bcryptjs');
-    data.password = await bcrypt.hash(data.password, 10);
+    const userObj = user.toObject();
+    userObj.id = userObj._id;
+    delete userObj._id;
+    res.json(userObj);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching user' });
   }
+};
 
-  const user = await prisma.user.update({
-    where: { id: userId },
-    data
-  });
-
-  res.json(user);
-});
+// Update user profile
+export const updateUserProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const data = { ...req.body };
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+    const user = await User.findByIdAndUpdate(
+        userId,
+        data,
+        { new: true, fields: { _id: 1, email: 1, name: 1 } }
+    );
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const userObj = user.toObject();
+    userObj.id = userObj._id;
+    delete userObj._id;
+    res.json(userObj);
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating user' });
+  }
+};
