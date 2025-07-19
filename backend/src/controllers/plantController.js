6a21701383396f77569102c334097821d0fe790b
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-19 15:12:15 +0530

    created getBestSellerPlants function

--- backend/src/controllers/plantController.js
+++ backend/src/controllers/plantController.js
@@ -1,5 +1,5 @@
 import {Plant} from '../models/model.js';
-import { getObjectURL} from '../utils/s3Utils.js';
+import {getObjectURL} from '../utils/s3Utils.js';
 
 // UTIL: Generates a signed S3 URL for a Plant image, if available
 const withImageUrl = async (plantDoc) => {
@@ -32,6 +32,18 @@ export const getTrendingPlants = async (req, res) => {
     res.json(plantsWithUrls);
   } catch (err) {
     res.status(500).json({ message: 'Error fetching trending plants' });
+  }
+};
+
+// Get bestseller plants
+export const getBestSellerPlants = async (req, res) => {
+  try {
+    const plants = await Plant.find({ isBestSeller: true })
+        .populate('categoryId');
+    const plantsWithUrls = await Promise.all(plants.map(withImageUrl));
+    res.json(plantsWithUrls);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching bestsellers' });
   }
 };
 
