4908e0373ae1e89a644c2290a31a8b7afa004586
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-19 14:03:22 +0530

    created getPlantsByCategory function

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -1,4 +1,4 @@
-import {Category} from "../models/model.js";
+import {Category, Plant} from "../models/model.js";
 
 
 export const getAllCategories = async (req, res) => {
@@ -9,3 +9,19 @@ export const getAllCategories = async (req, res) => {
     res.status(500).json({ message: 'Server Error', error: error.message });
   }
 };
+
+export const getPlantsByCategory = async (req, res) => {
+  try {
+    const { id: categoryId } = req.params;
+    const plants = await Plant.find({ category: categoryId }).populate('images');
+    res.json(plants);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
+
+
+
+
+
+
