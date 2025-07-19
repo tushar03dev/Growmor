c19421d2c9abee11573432f7d360489b04429c27
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-07-19 14:02:39 +0530

    created getAllCategories function

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -1,44 +1,11 @@
-
-const prisma = require('../utils/prismaImport.js');
-const asyncHandler = require('../utils/asyncHandler.js');
-
-exports.getAllCategories = asyncHandler(async (req, res) => {
-  const categories = await prisma.category.findMany();
-  res.json(categories);
-});
-
-exports.getPlantsByCategory = asyncHandler(async (req, res) => {
-  const categoryId = parseInt(req.params.id);
-  const plants = await prisma.plant.findMany({
-    where: { categoryId },
-    include: { images: true }
-  });
-  res.json(plants);
-});
+import {Category} from "../models/model.js";
 
 
-//ADMIN ONLY
-
-exports.createCategory = asyncHandler(async (req, res) => {
-  const { name, description } = req.body;
-  const category = await prisma.category.create({
-    data: { name , description}
-  });
-  res.status(201).json(category);
-});
-
-exports.updateCategory = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const data = req.body;
-  const category = await prisma.category.update({
-    where: { id: parseInt(id) },
-    data
-  });
-  res.json(category);
-});
-
-exports.deleteCategory = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  await prisma.category.delete({ where: { id: parseInt(id) } });
-  res.json({ message: 'Category deleted' });
-});
+export const getAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error', error: error.message });
+  }
+};
